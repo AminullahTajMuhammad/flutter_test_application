79d4721883a3b82108aa9ae0382e349f842e13b5
amin <aminullahtajmuhammad@gmail.com>
2019-10-24 03:53:55 +0500

    Learning Button Dialogs and Rows and Column

--- lib/first_screen/first_screen_code.dart
+++ lib/first_screen/first_screen_code.dart
@@ -8,15 +8,112 @@ class FirstScreenCode extends StatelessWidget {
 
     return Center(
       child: Container(
-        child: Text(
-          "Hello World",
-          style: TextStyle(
-              color: Colors.amberAccent,
-              fontWeight: FontWeight.bold
-          ),
+        padding: EdgeInsets.all(30.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Text(
+                      "Amin Taj Muh",
+                      style: TextStyle(
+                          color: Colors.amberAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0
+                      ),
+                    )
+                ),
+                Expanded(
+                    child: Text(
+                      "Amin Taj Muh",
+                      style: TextStyle(
+                          color: Colors.amberAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0
+                      ),
+                    )
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Text(
+                      "Amin Taj Muh",
+                      style: TextStyle(
+                          color: Colors.amberAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0
+                      ),
+                    )
+                ),
+                Expanded(
+                    child: Text(
+                      "Amin Taj Muh",
+                      style: TextStyle(
+                          color: Colors.amberAccent,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30.0
+                      ),
+                    )
+                ),
+              ],
+            ),
+            ShoppingAssetImage(),
+            FlightBookButton()
+          ],
         ),
-        alignment: Alignment.center,
         color: Colors.deepPurple,
+        alignment: Alignment.center,
+
+//        child: Row(
+//          children: <Widget>[
+//            Expanded(
+//                child: Text(
+//                  "Hello World Tere waste",
+//                  style: TextStyle(
+//                      color: Colors.amberAccent,
+//                      fontWeight: FontWeight.bold,
+//                      fontSize: 30.0
+//                  ),
+//                )
+//            ),
+//            Expanded(
+//              child: Text(
+//                  "Amin Taj Muh",
+//                  style: TextStyle(
+//                      color: Colors.amberAccent,
+//                      fontWeight: FontWeight.bold,
+//                      fontSize: 30.0
+//                  ),
+//                )
+//            ),
+//          ],
+//        ),
+//        child: Column(
+//          children: <Widget>[
+//            Expanded(
+//                child:Text(
+//                  "Amin Taj Muh",
+//                  style: TextStyle(
+//                      color: Colors.amberAccent,
+//                      fontWeight: FontWeight.bold,
+//                      fontSize: 30.0
+//                  ),
+//                ),
+//            ),
+//            Expanded(
+//                child: Text(
+//                    "Saqi Oh Saqi",
+//                    style: TextStyle(
+//                      color: Colors.amberAccent,
+//                      fontWeight: FontWeight.bold,
+//                      fontSize: 30.0
+//                    ),
+//                ),
+//            )
+//          ],
+//        ),
       )
     );
   }
@@ -26,5 +123,45 @@ class FirstScreenCode extends StatelessWidget {
     int number = random.nextInt(20);
     return number;
   }
+}
 
+class ShoppingAssetImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage("images/shopping_two.jpg");
+    Image image = Image(image: assetImage, height: 200.0, width: 250.0,);
+
+    return Container(
+      child: image,
+    );
+  }
+}
+
+class FlightBookButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: RaisedButton(
+          color: Colors.blue,
+          textColor: Colors.white,
+          elevation: 6.0,
+          child: Text("Book Now"),
+          onPressed: () {
+            bookFlight(context);
+          }
+        )
+    );
+  }
+
+  void bookFlight(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text("Flight Booked"),
+      content: Text("You are booked a new flight"),
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => alertDialog
+    );
+  }
 }
---
+++ lib/image.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'first_screen/first_screen_code.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        title: "Hello World",
+        home: FirstScreenCode(),
+      )
+  );
+}
